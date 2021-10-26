5e74ff222355336e7c4de5d5f9bdf1d1956049bd
frankietrimboli <62105951+frankietrimboli@users.noreply.github.com>
2021-10-26 10:40:38 -0400

    Add files via upload

---
+++ JavaScript/sandbox.js
@@ -0,0 +1,111 @@
+/*                                    CONSOLES
+// storing things using log and const in consoles
+let age = 25;
+let year = 2019;
+
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+console.log(points);
+
+var score = 75;
+console.log(score);
+
+*/
+
+
+/*                                      STRINGS
+
+//strings
+console.log('hello, world');
+
+let email = 'mario@thenetninja.co.uk';
+console.log(email);
+
+//string concatenation
+let firstName = 'Brandon';
+let lastName = 'Sanderson';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+//getting characters
+console.log(fullName[0]);
+
+//string length
+console.log(fullName.length);
+
+//string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName)
+
+let index = email.indexOf('@');
+console.log(index);
+
+*/
+
+/*                                      MORE STRINGS
+//common string methods
+
+let email = 'mario@thenetninja.co.uk';
+
+// let result = email.lastIndexOf('n');
+// let result = email.slice(0,10);
+// let result = email.substr(4,10);
+// let result = email.replace('m', 'w');
+// let result = email.replace('n', 'w');
+
+console.log(result);
+*/
+
+
+
+ let radius = 10;
+ const pi = 3.14;
+
+ // console.log(radius, pi);
+
+//math operators + , - , * , / , ** , %
+
+// console.log(10 / 2);
+// let result = radius % 3;
+// let result = pi * radius**2
+
+// order of operation - P E D M A S
+
+// let result = 5 * (10-3)**2
+
+// console.log(result);
+
+let likes = 10;
+
+// likes = likes + 1;      equal to line below
+// likes++;
+// likes = likes - 1;      equal to line below
+// likes--;
+
+// likes+= 10;
+// likes -5=;
+//likes *- 2;
+// likes /= 2;
+// console.log(likes);
+
+//NAN - not a number
+// console.log(5 / 'hello');
+// console.log(5 * 'hello');
+
+let result = ' the blog has ' + likes + ' likes';
+console.log(result);
+
+
+
+
+
+
+
+
+//f
